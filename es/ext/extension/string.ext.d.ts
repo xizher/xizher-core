fb2ab2e3e382fc952ab4e5fd3146623e9ecd8e00
xizher <xizher@163.com>
2021-05-23 21:13:12 +0800

    chore: build

--- es/ext/extension/string.ext.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IStringExtension {
-    trimAll(): string;
-    toDate(): Date;
-    toDateFormat(fmt: string): string;
-}
-declare function extString(str: string): IStringExtension;
-export default extString;
